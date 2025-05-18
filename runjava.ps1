b72a91ce653b476d6cfdad3fecfa38c49ac3fbb2
afrut19 <25942990+eric373@users.noreply.github.com>
2025-05-18 00:44:15 -0400

    Reorganized

--- runjava.ps1
+++ runjava.ps1
@@ -3,6 +3,6 @@ if ($name -eq $null) {
     Write-Host "Please provide a name as an argument."
     exit
 }
-javac ./src/$name.java -d .
+javac ./src/java/$name.java -d .
 java $name
 Get-ChildItem -Path . -Filter *.class -Recurse | Remove-Item -Force
