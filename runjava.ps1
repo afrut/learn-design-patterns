a1f49e365d1f2f12e182f4eeafaf062d241fb209
afrut19 <25942990+eric373@users.noreply.github.com>
2025-05-18 01:30:58 -0400

    Reorganized

--- runjava.ps1
+++ runjava.ps1
@@ -1,8 +1,9 @@
-$name = $args[0]
-if ($name -eq $null) {
-    Write-Host "Please provide a name as an argument."
+$filepath = $args[0]
+if ($filepath -eq $null) {
+    Write-Host "Please provide a filepath as an argument."
     exit
 }
-javac ./src/java/$name.java -d .
+$name = [System.IO.Path]::GetFileNameWithoutExtension($filepath)
+javac $filepath -d .
 java $name
 Get-ChildItem -Path . -Filter *.class -Recurse | Remove-Item -Force
