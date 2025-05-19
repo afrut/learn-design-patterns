2913855917a8b11bc81c1d9c6aa5d057530a687c
afrut19 <25942990+eric373@users.noreply.github.com>
2025-05-19 13:03:14 -0400

    Comment

--- src/java/Factory/SimpleFactory/Main.java
+++ src/java/Factory/SimpleFactory/Main.java
@@ -9,6 +9,8 @@ public class Main {
         Account buyer2 = new Account(1, new BigDecimal(3000));
         Account seller1 = new Account(2, new BigDecimal(2673.10));
         Broker b = new Broker(1);
+
+        // IMPROVED: Function signature is improved and factory is used
         b.transact(TransactionFactory.createTransaction("BUY", new BigDecimal(100), buyer1, new BigDecimal(10)));
         b.transact(TransactionFactory.createTransaction("SELL", new BigDecimal(100), seller1, new BigDecimal(20)));
         b.transact(TransactionFactory.createTransaction("DEPOSIT", new BigDecimal(200), buyer2));
